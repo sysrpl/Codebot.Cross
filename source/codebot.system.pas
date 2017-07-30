5745d3d070e862795b25387d2a18ae97674fe079
sysrpl <sysrpl@gmail.com>
2017-07-30 16:47:26 -0400

    Changes to generic containers

--- source/codebot.system.pas
+++ source/codebot.system.pas
@@ -2,7 +2,7 @@
 (*                                                      *)
 (*  Codebot Pascal Library                              *)
 (*  http://cross.codebot.org                            *)
-(*  Modified November 2015                              *)
+(*  Modified July 2017                                  *)
 (*                                                      *)
 (********************************************************)
 
@@ -100,6 +100,7 @@ type
     function GetCurrent: T;
     function MoveNext: Boolean;
     procedure Reset;
+    property Current: T read GetCurrent;
   end;
 {doc on}
 
@@ -109,15 +110,17 @@ type
 
 { TArrayList\<T\> is a simple extension to dynamic arrays
   See also
-  <link Overview.Codebot.System.TArrayList, TArrayList\<T\> members> }
+  <link Overview.Bare.System.TArrayList, TArrayList\<T\> members> }
 
   TArrayList<T> = record
-  public
+  public type
     {doc ignore}
-    type TArrayListEnumerator = class(TArrayEnumerator<T>) end;
+    TArrayListEnumerator = class(TArrayEnumerator<T>) end;
+    TCompareFunc = TCompare<T>;
     { Get the enumerator for the list }
     function GetEnumerator: IEnumerator<T>;
   private
+    function CompareExists: Boolean;
     procedure QuickSort(Order: TSortingOrder; Compare: TCompare<T>; L, R: Integer);
     function GetIsEmpty: Boolean;
     function GetFirst: T;
@@ -127,13 +130,14 @@ type
     function GetLength: Integer;
     procedure SetLength(Value: Integer);
     function GetData: Pointer;
-     function GetItem(Index: Integer): T;
+    function GetItem(Index: Integer): T;
     procedure SetItem(Index: Integer; const Value: T);
   public
     class var DefaultCompare: TCompare<T>;
     class var DefaultConvertString: TConvertString<T>;
     { The array acting as a list }
     var Items: TArray<T>;
+    class function ArrayOf(const Items: array of T): TArrayList<T>; static;
     class function Convert: TArrayList<T>; static;
     { Convert a list to an array }
     class operator Implicit(const Value: TArrayList<T>): TArray<T>;
@@ -141,6 +145,10 @@ type
     class operator Implicit(const Value: TArray<T>): TArrayList<T>;
     { Convert an open array to a list }
     class operator Implicit(const Value: array of T): TArrayList<T>;
+    { Performs a simple safe copy of up to N elements }
+    procedure Copy(out List: TArrayList<T>; N: Integer);
+    { Performs a fast unsafe copy of up to N elements }
+    procedure CopyFast(out List: TArrayList<T>; N: Integer);
     { Returns the lower bounds of the list }
     function Lo: Integer;
     { Returns the upper bounds of the list }
@@ -168,7 +176,9 @@ type
     { Sort the items using a comparer }
     procedure Sort(Order: TSortingOrder = soAscend; Comparer: TCompare<T> = nil);
     { Attempt to find the item using DefaultCompare }
-    function IndexOf(const Item: T): Integer;
+    function IndexOf(const Item: T): Integer; overload;
+    { Attempt to find the item using a supplied comparer }
+    function IndexOf(const Item: T; Comparer: TCompare<T>): Integer; overload;
     { Join a the array into a string using a separator }
     function Join(const Separator: string; Convert: TConvertString<T> = nil): string;
     { Returns true if ther are no items in the list }
@@ -185,23 +195,96 @@ type
     property Item[Index: Integer]: T read GetItem write SetItem; default;
   end;
 
+{ TMap\<K, V\> is a array like simple dictionary
+  See also
+  <link Overview.Bare.System.TMap, TMap\<K, V\> members> }
+
+  TMap<K, V> = record
+  private
+    FKeys: TArrayList<K>;
+    FValues: TArrayList<V>;
+    function GetItem(const Key: K): V;
+    procedure SetItem(const Key: K; const Value: V);
+  public
+    { Get or set and item using a key }
+    property Item[const Key: K]: V read GetItem write SetItem;
+  end;
+
+{ TBaseGrowList }
+
+  TBaseList = class
+  public
+    constructor Create(N: Integer = 0); virtual;
+  end;
+
+  TBaseListClass = class of TBaseList;
+
+{ TGrowList\<T\> is a class for incrementally adding large amounts of growing data
+  See also
+  <link Overview.Codebot.System.TGrowList\<T\>, TGrowList\<T\> members> }
+
+  TGrowList<T> = class(TBaseList)
+  private
+    FBuffer: TArrayList<T>;
+    FCount: Integer;
+    FLength: Integer;
+    procedure Grow(N: Integer);
+    function GetData(Index: Integer): Pointer;
+    function GetItem(Index: Integer): T;
+    procedure SetItem(Index: Integer; Value: T);
+  protected
+    procedure Added(N: Integer); virtual;
+  public
+    { Create a new dynamic buffer optionally allocating room for a N number
+      of future items }
+    constructor Create(N: Integer = 0); override;
+    { Remove any extra data allocated by the previous grow }
+    procedure Pack;
+    { Create a copy of the list }
+    function Clone: TObject; virtual;
+    { Add a range of items to the list }
+    procedure AddRange(const Range: array of T);
+    { Add a single item to the list }
+    procedure AddItem(const Item: T);
+    { Clear the buffer optionally allocating room for a N number
+      of future items }
+    procedure Clear(N: Integer = 0);
+    { Pointer to the data at a specified index }
+    property Data[Index: Integer]: Pointer read GetData; default;
+    { Item at specified index }
+    property Item[Index: Integer]: T read GetItem write SetItem;
+    { The number of items in the list }
+    property Count: Integer read FCount;
+  end;
+
 {doc off}
   StringArray = TArrayList<string>;
+  WordArray = TArrayList<Word>;
   IntArray = TArrayList<Integer>;
   Int64Array = TArrayList<Int64>;
   FloatArray = TArrayList<Float>;
   BoolArray = TArrayList<Boolean>;
+  PointerArray = TArrayList<Pointer>;
+  ObjectArray = TArrayList<TObject>;
+  InterfaceArray = TArrayList<IInterface>;
 
 function DefaultStringCompare(constref A, B: string): Integer;
 function DefaultStringConvertString(constref Item: string): string;
+function DefaultWordCompare(constref A, B: Word): Integer;
+function DefaultWordConvertString(constref Item: Word): string;
 function DefaultIntCompare(constref A, B: Integer): Integer;
 function DefaultIntConvertString(constref Item: Integer): string;
 function DefaultInt64Compare(constref A, B: Int64): Integer;
 function DefaultInt64ConvertString(constref Item: Int64): string;
 function DefaultFloatCompare(constref A, B: Float): Integer;
 function DefaultFloatConvertString(constref Item: Float): string;
+function DefaultObjectCompare(constref A, B: TObject): Integer;
+function DefaultInterfaceCompare(constref A, B: IInterface): Integer;
+function DefaultCompare8(constref A, B: Byte): Integer;
+function DefaultCompare16(constref A, B: Word): Integer;
+function DefaultCompare32(constref A, B: LongWord): Integer;
+function DefaultCompare64(constref A, B: LargeWord): Integer;
 {doc on}
-
 {$endregion}
 
 {$region math routines}
@@ -626,10 +709,6 @@ const
 function FindOpen(const Path: string; Attr: Longint; out Search: TSearchRec): LongInt;
 { Find file system items from a path outputting to a TStrings object }
 procedure FindFiles(const Path: string; out FileSearch: TStrings; Attributes: Integer = 0); overload;
-
-{ TNamedValues\<T\> is a simple case insensitive string based dictionary
-  See also
-  <link Overview.Codebot.System.TNamedValues, TNamedValues\<T\> members> }
 
 type
   TFileSearchItem = record
@@ -3041,6 +3120,41 @@ begin
     Result.Push(I);
 end;
 
+class function TArrayList<T>.ArrayOf(const Items: array of T): TArrayList<T>;
+var
+  I: T;
+begin
+  for I in Items do
+    Result.Push(I);
+end;
+
+procedure TArrayList<T>.Copy(out List: TArrayList<T>; N: Integer);
+var
+  I: Integer;
+begin
+  if N < 1 then
+    N := Length
+  else if N > Length then
+    N := Length;
+  List.Length := N;
+  if N < 1 then
+    Exit;
+  for I := 0 to N - 1 do
+    List.Items[I] := Items[I];
+end;
+
+procedure TArrayList<T>.CopyFast(out List: TArrayList<T>; N: Integer);
+begin
+  if N < 1 then
+    N := Length
+  else if N > Length then
+    N := Length;
+  List.Length := N;
+  if N < 1 then
+    Exit;
+  System.Move(Items[0], List.Items[0], N * SizeOf(T));
+end;
+
 procedure TArrayList<T>.Reverse;
 var
   Swap: T;
@@ -3131,7 +3245,7 @@ begin
     Result := Pop
   else
   begin
-    I := Random(I);
+    I := System.Random(I);
     Result := Items[I];
     Delete(I);
   end;
@@ -3147,8 +3261,8 @@ begin
   for I := 0 to System.Length(Items) - 1 do
     if Func(Items[I]) then
     begin
-      Result.Items[J] := Items[I];
-      Inc(J);
+   Result.Items[J] := Items[I];
+   Inc(J);
     end;
   System.SetLength(Result.Items, J);
 end;
@@ -3159,7 +3273,7 @@ var
 begin
   for I := 0 to System.Length(Items) - 1 do
     if Func(Items[I]) then
-      Exit(Items[I]);
+   Exit(Items[I]);
   Result := Default(T);
 end;
 
@@ -3178,31 +3292,221 @@ begin
   Length := 0;
 end;
 
+{ TMap<K, V> }
+
+function TMap<K, V>.GetItem(const Key: K): V;
+var
+  I: Integer;
+begin
+  I := FKeys.IndexOf(Key);
+  if I > -1 then
+    Result := FValues.Items[I]
+  else
+    Result := Default(V);
+end;
+
+procedure TMap<K, V>.SetItem(const Key: K; const Value: V);
+var
+  I: Integer;
+begin
+  I := FKeys.IndexOf(Key);
+  if I > -1 then
+    FValues.Items[I] := Value
+  else
+  begin
+    FKeys.Push(Key);
+    FValues.Push(Value);
+  end;
+end;
+
+constructor TBaseList.Create(N: Integer = 0);
+begin
+  inherited Create;
+end;
+
+{ TGrowList<T> }
+
+constructor TGrowList<T>.Create(N: Integer = 0);
+begin
+  inherited Create(N);
+  Clear(N);
+end;
+
+procedure TGrowList<T>.Pack;
+begin
+  if FCount < FLength then
+  begin
+    FLength := FCount;
+    FBuffer.Length := FLength;
+  end;
+end;
+
+function TGrowList<T>.Clone: TObject;
+var
+  Copy: TGrowList<T>;
+begin
+  Copy := TBaseListClass(ClassType).Create as TGrowList<T>;
+  if FCount = 0 then
+    Exit(Copy);
+  Copy.FCount := FCount;
+  Copy.FLength := FCount;
+  FBuffer.CopyFast(Copy.FBuffer, FCount);
+  Result := Copy;
+end;
+
+procedure TGrowList<T>.Added(N: Integer);
+begin
+end;
+
+procedure TGrowList<T>.Grow(N: Integer);
+const
+  MaxGrowSize = 50000;
+var
+  C: Integer;
+begin
+  if N < 1 then
+    Exit;
+  if N < 16 then
+    N := 16;
+  if N + FCount > FLength then
+    if FLength = 0 then
+    begin
+      FLength := N;
+      FBuffer.Length := N;
+    end
+    else
+    begin
+      if FLength > MaxGrowSize then
+      begin
+        if N < MaxGrowSize then
+        C := MaxGrowSize
+        else
+        C := N;
+        C := FLength + C;
+      end
+      else
+      begin
+        C := FLength + MaxGrowSize;
+        C := FLength * 2;
+        FLength := FLength + N;
+        while C < FLength do
+          C := C * 2;
+      end;
+     FLength := C;
+     FBuffer.Length := C;
+    end;
+end;
+
+procedure TGrowList<T>.Clear(N: Integer = 0);
+begin
+  FCount := 0;
+  if N = 0 then
+  begin
+    FLength := 0;
+    FBuffer.Length := 0
+  end
+  else if N > FBuffer.Length then
+    Grow(N - FBuffer.Length);
+  Added(0);
+end;
+
+procedure TGrowList<T>.AddRange(const Range: array of T);
+var
+  I, J: Integer;
+begin
+  I := Length(Range);
+  if I < 1 then
+    Exit;
+  Grow(I);
+  for J := 0 to I - 1 do
+    FBuffer.Items[FCount + J] := Range[J];
+  Inc(FCount, I);
+  Added(I);
+end;
+
+procedure TGrowList<T>.AddItem(const Item: T);
+begin
+  Grow(1);
+  FBuffer.Items[FCount] := Item;
+  Inc(FCount);
+  Added(1);
+end;
+
+function TGrowList<T>.GetData(Index: Integer): Pointer;
+begin
+  Result := @FBuffer.Items[Index];
+end;
+
+function TGrowList<T>.GetItem(Index: Integer): T;
+begin
+  Result := FBuffer.Items[Index];
+end;
+
+procedure TGrowList<T>.SetItem(Index: Integer; Value: T);
+begin
+  FBuffer.Items[Index] := Value;
+end;
+
+{ Compare functions }
+
+function DefaultCompare8(constref A, B: Byte): Integer;
+begin
+  Result := B - A;
+end;
+
+function DefaultCompare16(constref A, B: Word): Integer;
+begin
+  Result := B - A;
+end;
+
+function DefaultCompare32(constref A, B: LongWord): Integer;
+begin
+  Result := B - A;
+end;
+
+function DefaultCompare64(constref A, B: LargeWord): Integer;
+begin
+  Result := B - A;
+end;
+
+function TArrayList<T>.CompareExists: Boolean;
+begin
+  if Assigned(DefaultCompare) then
+    Exit(True);
+  case SizeOf(T) of
+    8: DefaultCompare := TCompareFunc(DefaultCompare8);
+    16: DefaultCompare := TCompareFunc(DefaultCompare16);
+    32: DefaultCompare := TCompareFunc(DefaultCompare32);
+    64: DefaultCompare := TCompareFunc(DefaultCompare64);
+  end;
+  Result := Assigned(DefaultCompare);
+end;
+
 procedure TArrayList<T>.QuickSort(Order: TSortingOrder; Compare: TCompare<T>; L, R: Integer);
 var
   F, I, J, P: Integer;
 begin
   repeat
     if Order = soDescend then
-      F := -1
+   F := -1
     else
-      F := 1;
+   F := 1;
     I := L;
     J := R;
     P := (L + R) shr 1;
     repeat
-      while Compare(Items[I], Items[P]) * F < 0 do Inc(I);
-      while Compare(Items[J], Items[P]) * F > 0 do Dec(J);
-      if I <= J then
-      begin
-        Exchange(I, J);
-        if P = I then
-          P := J
-        else if P = J then
-          P := I;
-        Inc(I);
-        Dec(J);
-      end;
+   while Compare(Items[I], Items[P]) * F < 0 do Inc(I);
+   while Compare(Items[J], Items[P]) * F > 0 do Dec(J);
+   if I <= J then
+   begin
+  Exchange(I, J);
+  if P = I then
+    P := J
+  else if P = J then
+    P := I;
+  Inc(I);
+  Dec(J);
+   end;
     until I > J;
     if L < J then QuickSort(Order, Compare, L, J);
     L := I;
@@ -3220,7 +3524,7 @@ begin
     Exit;
   if Assigned(Comparer) then
     QuickSort(Order, Comparer, 0, I - 1)
-  else if Assigned(DefaultCompare) then
+  else if CompareExists then
     QuickSort(Order, DefaultCompare, 0, I - 1);
 end;
 
@@ -3230,10 +3534,22 @@ var
 begin
   Result := -1;
   I := Length;
-  if (I > 0) and Assigned(DefaultCompare) then
+  if (I > 0) and CompareExists then
     for I := Lo to Hi do
       if DefaultCompare(Item, Items[I]) = 0 then
-        Exit(I);
+  Exit(I);
+end;
+
+function TArrayList<T>.IndexOf(const Item: T; Comparer: TCompare<T>): Integer;
+var
+  I: Integer;
+begin
+  Result := -1;
+  I := Length;
+  if I > 0 then
+    for I := Lo to Hi do
+   if Comparer(Item, Items[I]) = 0 then
+  Exit(I);
 end;
 
 function TArrayList<T>.Join(const Separator: string; Convert: TConvertString<T> = nil): string;
@@ -3247,13 +3563,13 @@ begin
   begin
     Result := Convert(First);
     for I := Low(Items) + 1 to High(Items) do
-      Result := Result + Separator + Convert(Items[I]);
+   Result := Result + Separator + Convert(Items[I]);
   end
   else if Assigned(DefaultConvertString) then
   begin
     Result := DefaultConvertString(First);
     for I := Low(Items) + 1 to High(Items) do
-      Result := Result + Separator + DefaultConvertString(Items[I]);
+   Result := Result + Separator + DefaultConvertString(Items[I]);
   end;
 end;
 
@@ -3264,22 +3580,30 @@ end;
 
 function TArrayList<T>.GetFirst: T;
 begin
-  Result := Items[0];
+  if Length > 0 then
+    Result := Items[0]
+  else
+    Result := Default(T);
 end;
 
 procedure TArrayList<T>.SetFirst(const Value: T);
 begin
-  Items[0] := Value;
+  if Length > 0 then
+    Items[0] := Value;
 end;
 
 function TArrayList<T>.GetLast: T;
 begin
-  Result := Items[Length - 1];
+  if Length > 0 then
+    Result := Items[Length - 1]
+  else
+    Result := Default(T);
 end;
 
 procedure TArrayList<T>.SetLast(const Value: T);
 begin
-  Items[Length - 1] := Value;
+  if Length > 0 then
+    Items[Length - 1] := Value;
 end;
 
 function TArrayList<T>.GetLength: Integer;
@@ -3311,6 +3635,73 @@ class function TArrayList<T>.Convert: TArrayList<T>;
 begin
   Result.Length := 0;
 end;
+
+function DefaultStringCompare(constref A, B: string): Integer;
+begin
+  Result := StrCompare(A, B);
+end;
+
+function DefaultStringConvertString(constref Item: string): string;
+begin
+  Result := Item;
+end;
+
+function DefaultWordCompare(constref A, B: Word): Integer;
+begin
+  Result := B - A;
+end;
+
+function DefaultWordConvertString(constref Item: Word): string;
+begin
+  Result := IntToStr(Item);
+end;
+
+function DefaultIntCompare(constref A, B: Integer): Integer;
+begin
+  Result := B - A;
+end;
+
+function DefaultIntConvertString(constref Item: Integer): string;
+begin
+  Result := IntToStr(Item);
+end;
+
+function DefaultInt64Compare(constref A, B: Int64): Integer;
+begin
+  Result := B - A;
+end;
+
+function DefaultInt64ConvertString(constref Item: Int64): string;
+begin
+  Result := IntToStr(Item);
+end;
+
+function DefaultFloatCompare(constref A, B: Float): Integer;
+begin
+  if A < B then
+    Result := -1
+  else if A > B then
+    Result := 1
+  else
+    Result := 0;
+end;
+
+function DefaultFloatConvertString(constref Item: Float): string;
+begin
+  Result := FloatToStr(Item);
+end;
+
+function DefaultObjectCompare(constref A, B: TObject): Integer;
+begin
+  Result := IntPtr(A) - IntPtr(B);
+end;
+
+function DefaultInterfaceCompare(constref A, B: IInterface): Integer;
+begin
+  Result := IntPtr(A) - IntPtr(B);
+end;
+
+{$endregion}
 
 { TNamedValues<T> }
 
@@ -3885,55 +4276,12 @@ begin
 end;
 {$endregion}
 
-function DefaultStringCompare(constref A, B: string): Integer;
-begin
-  Result := StrCompare(A, B);
-end;
-
-function DefaultStringConvertString(constref Item: string): string;
-begin
-  Result := Item;
-end;
-
-function DefaultIntCompare(constref A, B: Integer): Integer;
-begin
-  Result := B - A;
-end;
-
-function DefaultIntConvertString(constref Item: Integer): string;
-begin
-  Result := IntToStr(Item);
-end;
-
-function DefaultInt64Compare(constref A, B: Int64): Integer;
-begin
-  Result := B - A;
-end;
-
-function DefaultInt64ConvertString(constref Item: Int64): string;
-begin
-  Result := IntToStr(Item);
-end;
-
-function DefaultFloatCompare(constref A, B: Float): Integer;
-begin
-  if A < B then
-    Result := -1
-  else if A > B then
-    Result := 1
-  else
-    Result := 0;
-end;
-
-function DefaultFloatConvertString(constref Item: Float): string;
-begin
-  Result := FloatToStr(Item);
-end;
-
 initialization
   @LibraryExceptproc := @LibraryExcept;
   StringArray.DefaultCompare := DefaultStringCompare;
   StringArray.DefaultConvertString := DefaultStringConvertString;
+  WordArray.DefaultCompare := DefaultWordCompare;
+  WordArray.DefaultConvertString := DefaultWordConvertString;
   IntArray.DefaultCompare := DefaultIntCompare;
   IntArray.DefaultConvertString := DefaultIntConvertString;
   Int64Array.DefaultCompare := DefaultInt64Compare;
@@ -3941,6 +4289,4 @@ initialization
   FloatArray.DefaultCompare := DefaultFloatCompare;
   FloatArray.DefaultConvertString := DefaultFloatConvertString;
 end.
-
-
 
